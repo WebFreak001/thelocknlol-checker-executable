93a50032f72ac242e090f27667de9d0fe074bca7
WebFreak001 <janju007@web.de>
2014-01-30 15:32:56 +0100

    Auto-Formatted Code

--- WinForms/Forms/OptionsForm.cs
+++ WinForms/Forms/OptionsForm.cs
@@ -28,8 +28,8 @@ namespace WinForms.Forms
 		public void tryFile(string f, bool mayPick)
 		{
 			List<ListViewItem> items = new List<ListViewItem>();
-			foreach(ListViewItem i in lvSounds.Items) items.Add(i);
-			if(items.Count(l => l.Text.ToLower().Trim() == f.ToLower().Trim()) > 0) return;
+			foreach (ListViewItem i in lvSounds.Items) items.Add(i);
+			if (items.Count(l => l.Text.ToLower().Trim() == f.ToLower().Trim()) > 0) return;
 
 			bool searchForFile = false;
 			if (f == "")
@@ -39,29 +39,29 @@ namespace WinForms.Forms
 			else if (!File.Exists(f)) searchForFile = true;
 			if (searchForFile)
 			{
-				if(mayPick)
+				if (mayPick)
 				{
 					OpenFileDialog d = new OpenFileDialog();
 					string dir = "";
-					if(f.Trim() != "" && (f.Contains("/") || f.Contains("\\"))) dir = Path.GetFullPath(Path.GetDirectoryName(f));
-					if(Directory.Exists(dir)) d.InitialDirectory = dir;
+					if (f.Trim() != "" && (f.Contains("/") || f.Contains("\\"))) dir = Path.GetFullPath(Path.GetDirectoryName(f));
+					if (Directory.Exists(dir)) d.InitialDirectory = dir;
 					d.Filter = "Sound File (*.wav)|*.wav";
 					DialogResult r = d.ShowDialog();
-					if(r == DialogResult.OK)
+					if (r == DialogResult.OK)
 					{
-						 string file = d.FileName;
-						 Uri root = new Uri(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
-						 Uri target = new Uri(file);
-						 string relative = root.MakeRelativeUri(target).ToString();
-						 if(!relative.Contains("..")) file = relative;
-						 tbSoundFile.Text = file.Replace('\\', '/');
-						 tryFile(tbSoundFile.Text.Trim(), false);
+						string file = d.FileName;
+						Uri root = new Uri(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+						Uri target = new Uri(file);
+						string relative = root.MakeRelativeUri(target).ToString();
+						if (!relative.Contains("..")) file = relative;
+						tbSoundFile.Text = file.Replace('\\', '/');
+						tryFile(tbSoundFile.Text.Trim(), false);
 					}
 				}
 			}
 			else
 			{
-				if(f.Trim().ToLower().EndsWith(".wav"))
+				if (f.Trim().ToLower().EndsWith(".wav"))
 				{
 					tbSoundFile.Text = "";
 					lvSounds.Items.Add(new ListViewItem(f));
@@ -72,15 +72,15 @@ namespace WinForms.Forms
 
 		private void OptionsForm_Load(object sender, EventArgs e)
 		{
-			for(int i = 0; i < lvSounds.Items.Count; i++) cbSound.Items.Add(lvSounds.Items[i].Text);
+			for (int i = 0; i < lvSounds.Items.Count; i++) cbSound.Items.Add(lvSounds.Items[i].Text);
 			cbSound.SelectedIndex = 0;
 		}
 
 		private void tbSoundFile_TextChanged(object sender, EventArgs e)
 		{
 			List<ListViewItem> items = new List<ListViewItem>();
-			foreach(ListViewItem i in lvSounds.Items) items.Add(i);
-			if(items.Count(l => l.Text.ToLower().Trim() == tbSoundFile.Text.ToLower().Trim()) > 0) btnAddSound.Enabled = false;
+			foreach (ListViewItem i in lvSounds.Items) items.Add(i);
+			if (items.Count(l => l.Text.ToLower().Trim() == tbSoundFile.Text.ToLower().Trim()) > 0) btnAddSound.Enabled = false;
 			else btnAddSound.Enabled = true;
 			if (tbSoundFile.Text.Trim() == "") btnAddSound.Text = "Durchsuchen";
 			else
@@ -92,7 +92,7 @@ namespace WinForms.Forms
 
 		private void btnPreviewSound_Click(object sender, EventArgs e)
 		{
-			if(File.Exists(cbSound.Items[cbSound.SelectedIndex].ToString()))
+			if (File.Exists(cbSound.Items[cbSound.SelectedIndex].ToString()))
 			{
 				btnPreviewSound.Enabled = false;
 				SoundPlayer p = new SoundPlayer(cbSound.Items[cbSound.SelectedIndex].ToString());
@@ -106,15 +106,15 @@ namespace WinForms.Forms
 			else
 			{
 				List<ListViewItem> items = new List<ListViewItem>();
-				foreach(ListViewItem i in lvSounds.Items) items.Add(i);
+				foreach (ListViewItem i in lvSounds.Items) items.Add(i);
 				if (items.Count(l => l.Text.ToLower().Trim() == cbSound.Items[cbSound.SelectedIndex].ToString().ToLower().Trim()) > 0)
 				{
 					ListViewItem i = items.Where(l => l.Text.ToLower().Trim() == cbSound.Items[cbSound.SelectedIndex].ToString().ToLower().Trim()).FirstOrDefault();
 
 					DialogResult r = MessageBox.Show(cbSound.Items[cbSound.SelectedIndex] + " konnte nicht gefunden werden!\nDatei aus der Liste entfernen?", "Fehler", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-					if(r == DialogResult.Yes)
+					if (r == DialogResult.Yes)
 					{
-						if(lvSounds.Items.Contains(i)) lvSounds.Items.Remove(i);
+						if (lvSounds.Items.Contains(i)) lvSounds.Items.Remove(i);
 						else MessageBox.Show("...");
 					}
 				}
