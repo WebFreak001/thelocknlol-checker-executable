3a33518cc2ba468f6b4a4c9fbde3b2ed159d02b7
WebFreak001 <janju007@web.de>
2014-01-31 22:33:15 +0100

    Updated Config

--- WinForms/Config.cs
+++ WinForms/Config.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,18 +11,33 @@ namespace WinForms
 {
 	public class CheckerFormat
 	{
+		public bool Enabled { get; set; }
+		public string Name { get; set; }
 		public string Facebook { get; set; }
 		public string YouTube { get; set; }
 		public string Twitch { get; set; }
+		public string Twitter { get; set; }
+	}
+
+	public class SoundFormat
+	{
+		public bool OnVideo { get; set; }
+		public bool OnLivestream { get; set; }
+		public bool OnFacebook { get; set; }
+		public bool OnTwitter { get; set; }
 	}
 
 	public class ConfigFormat
 	{
-		public bool Video { get; set; }
-		public bool Livestream { get; set; }
-		public bool Facebook { get; set; }
+		public bool CheckVideo { get; set; }
+		public bool CheckLivestream { get; set; }
+		public bool CheckSocial { get; set; }
+		public bool MergeSocialVideo { get; set; }
 		public bool AutoUpdate { get; set; }
+		public SoundFormat Sounds { get; set; }
 		public string LastVideo { get; set; }
+		public string CurrentSound { get; set; }
+		public Color NotifyColor { get; set; }
 		public CheckerFormat[] Checkers { get; set; }
 	}
 
@@ -32,13 +48,13 @@ namespace WinForms
 
 		public static void Load()
 		{
-			if (!File.Exists("config.json")) File.WriteAllText("config.json", JsonConvert.SerializeObject(new ConfigFormat() { Facebook = true, LastVideo = "", Livestream = true, Video = true, Checkers = new[] { new CheckerFormat() { Facebook = "TheLockNLol", Twitch = "TheLockNLol", YouTube = "TheLockNLol" } } }));
+			if (!File.Exists("config.json")) File.WriteAllText("config.json", JsonConvert.SerializeObject(new ConfigFormat() { NotifyColor = Color.White, CurrentSound = "Sounds/pew.wav", CheckSocial = true, MergeSocialVideo = true, LastVideo = "", CheckLivestream = true, CheckVideo = true, AutoUpdate = false, Sounds = new SoundFormat() { OnFacebook = false, OnLivestream = true, OnTwitter = false, OnVideo = false }, Checkers = new[] { new CheckerFormat() { Enabled = true, Facebook = "TheLockNLol", Twitch = "TheLockNLol", YouTube = "TheLockNLol", Name = "TheLockNLol", Twitter = "TheLockNLol" } } }, Formatting.Indented));
 			Settings = JsonConvert.DeserializeObject<ConfigFormat>(File.ReadAllText("config.json"));
 		}
 
 		public static void Save()
 		{
-
+			File.WriteAllText("config.json", JsonConvert.SerializeObject(Settings, Formatting.Indented));
 		}
 	}
 }
