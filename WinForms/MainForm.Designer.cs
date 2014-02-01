53a1b6849e34b214e0177f0d8ed0406e73ba7ff8
WebFreak001 <janju007@web.de>
2014-02-01 13:33:48 +0100

    Added Filter to home UI

--- WinForms/MainForm.Designer.cs
+++ WinForms/MainForm.Designer.cs
@@ -30,11 +30,14 @@
 		{
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("TheLockNLol");
 			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
 			this.btnRefresh = new System.Windows.Forms.ToolStripButton();
 			this.btnOptions = new System.Windows.Forms.ToolStripButton();
 			this.layout = new System.Windows.Forms.FlowLayoutPanel();
 			this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.toolStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -73,9 +76,9 @@
 			// 
 			this.layout.AutoScroll = true;
 			this.layout.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.layout.Location = new System.Drawing.Point(0, 25);
+			this.layout.Location = new System.Drawing.Point(166, 25);
 			this.layout.Name = "layout";
-			this.layout.Size = new System.Drawing.Size(654, 422);
+			this.layout.Size = new System.Drawing.Size(488, 422);
 			this.layout.TabIndex = 1;
 			// 
 			// refreshTimer
@@ -83,12 +86,35 @@
 			this.refreshTimer.Interval = 10000;
 			this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
 			// 
+			// listView1
+			// 
+			this.listView1.CheckBoxes = true;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+			this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.listView1.GridLines = true;
+			listViewItem1.StateImageIndex = 0;
+			this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+			this.listView1.Location = new System.Drawing.Point(0, 25);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(166, 422);
+			this.listView1.TabIndex = 2;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Prüfe";
+			this.columnHeader1.Width = 160;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(654, 447);
 			this.Controls.Add(this.layout);
+			this.Controls.Add(this.listView1);
 			this.Controls.Add(this.toolStrip1);
 			this.Name = "MainForm";
 			this.Text = "TheLockNLol Checker";
@@ -108,6 +134,8 @@
 		private System.Windows.Forms.FlowLayoutPanel layout;
 		private System.Windows.Forms.ToolStripButton btnRefresh;
 		private System.Windows.Forms.Timer refreshTimer;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
 	}
 }
 
