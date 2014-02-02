86ae00e2284a0fc829bfce1c156cd800ee70f066
WebFreak001 <janju007@web.de>
2014-02-02 12:57:42 +0100

    Wrong Version download

--- WinForms/Version.cs
+++ WinForms/Version.cs
@@ -1,5 +1,5 @@
 ﻿public class Version
 {
 	public static int VersionNumber = 2;
-	public static string Download = "https://github.com/WebFreak001/thelocknlol-checker-executable/releases/tag/v1.0.2";
+	public static string Download = "https://github.com/WebFreak001/thelocknlol-checker-executable/releases/download/v1.0.2/TheLockNLol-Checker.zip";
 }
