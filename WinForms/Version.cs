08e4a5e3993c5ed7ead35f6a41a53507d595a2f9
WebFreak001 <janju007@web.de>
2014-02-02 12:42:07 +0100

    Ready for 1.0.2

--- WinForms/Version.cs
+++ WinForms/Version.cs
@@ -1,5 +1,5 @@
 ﻿public class Version
 {
-	public static int VersionNumber = 1;
-	public static string Download = "https://github.com/WebFreak001/thelocknlol-checker-executable/releases/tag/v1.0.1";
+	public static int VersionNumber = 2;
+	public static string Download = "https://github.com/WebFreak001/thelocknlol-checker-executable/releases/tag/v1.0.2";
 }
