e9a033daf6e62de5c2aa4f2c23ac7f393bba3e7b
WebFreak001 <janju007@web.de>
2014-02-07 16:54:40 +0100

    Changed version

--- WinForms/Version.cs
+++ WinForms/Version.cs
@@ -1,5 +1,5 @@
 ﻿public class Version
 {
-	public static int VersionNumber = 10;
-	public static string Download = "https://github.com/WebFreak001/thelocknlol-checker-executable/releases/download/v1.1.0/TheLockNLol-Checker.zip";
+	public static int VersionNumber = 11;
+	public static string Download = "https://github.com/WebFreak001/thelocknlol-checker-executable/releases/download/v1.1.1/TheLockNLol-Checker.zip";
 }
