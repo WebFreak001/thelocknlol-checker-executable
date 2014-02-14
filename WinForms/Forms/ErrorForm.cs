d7c3598d3850723edc76e08a4a22bae680bd192c
WebFreak001 <janju007@web.de>
2014-02-14 21:08:27 +0100

    Fixed ErrorForm

--- WinForms/Forms/ErrorForm.cs
+++ WinForms/Forms/ErrorForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,24 @@ namespace WinForms.Forms
 		{
 			InitializeComponent();
 			textBox1.Text = "TheLockNLol Checker wurde unerwartet beendet\n\nBitte senden Sie diesen Fehler an uns!\n\n\n=============================\n\n" + e.Message + "\n" + e.Source + "\n" + e.InnerException + "\n" + e.StackTrace;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			Process.Start("https://github.com/WebFreak001/thelocknlol-checker-executable/issues?state=open");
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
+			{
+				Application.OpenForms[i].Close();
+			}
 		}
 	}
 }
